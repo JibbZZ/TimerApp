28343fd54a8fbf41ef2af2cbb14a6c3655fb4c06
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2023-10-04 11:16:37 +0200

    Added navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { TimerScreen } from './TimerScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Timer">
+        <Stack.Screen name="Timer" component={TimerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
