221567b9a4c1e31d3c862d9e74a4384115cc6d5b
Jimmy Einarsson <Jimmyeinarssonn@hotmail.com>
2023-10-05 13:18:47 +0200

    Added LinearGradient (expo component)

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Button, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 
 function HomeScreen({ navigation }: any) {
-  return (
-    <View style={styles.container}>
-      <Button 
-        title="Timer" 
-        onPress={() => navigation.navigate('TimerClock')} 
-      />
-      <Button 
-        title="SetTimer" 
-        onPress={() => navigation.navigate('TimerSetScreen')} 
-      />
-    </View>
-  );
-}
+    return (
+      <LinearGradient colors={['blue', 'red']} style={styles.container}>
+        <Button 
+          title="Timer" 
+          onPress={() => navigation.navigate('TimerClock')}
+          color="white" 
+        />
+        <Button 
+          title="SetTimer" 
+          onPress={() => navigation.navigate('TimerSetScreen')} 
+          color="white"
+        />
+      </LinearGradient>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
